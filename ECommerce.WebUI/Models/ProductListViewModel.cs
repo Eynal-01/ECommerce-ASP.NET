3241d9cdada754991b9b8c5bbfa5efa28821df56
Eynal <Eynal@DESKTOP-I662UI3>
2023-11-25 15:29:25 +0400

    Search added successfully

--- ECommerce.WebUI/Models/ProductListViewModel.cs
+++ ECommerce.WebUI/Models/ProductListViewModel.cs
@@ -4,7 +4,7 @@ namespace ECommerce.WebUI.Models
 {
     public class ProductListViewModel
     {
-        public List<Product>? Products { get; set; }
+        public IEnumerable<Product>? Products { get; set; }
         public int CurrentCategory { get; set; }
         public int PageCount { get; set; }
         public int PageSize { get; set; }
